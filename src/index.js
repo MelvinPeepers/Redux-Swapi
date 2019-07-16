dd3f98b07e62c62dcd635acd6c5ea36f350f88f0
Melvin Peepers <peepersm@gmail.com>
2019-07-16 19:57:03 -0400

    added Redux DevTool

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
+// added compose
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-  applyMiddleware{
-    
-  },
+  rootReducer,
+  compose(
+    /* applyMiddleware goes here */
+    applyMiddleware(logger),
+
+    // Redux DevTool
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
 );
 
 ReactDOM.render(
