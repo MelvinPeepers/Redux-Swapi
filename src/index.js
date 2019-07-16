1898e8658493dd22b7fc3fe1a3b2f8e19ea74a05
Melvin Peepers <peepersm@gmail.com>
2019-07-16 19:39:05 -0400

    setup applyMiddleware and logger

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -14,6 +14,9 @@ import rootReducer from "./reducers";
 const store = createStore(
   rootReducer
   /* applyMiddleware goes here */
+  applyMiddleware{
+    
+  },
 );
 
 ReactDOM.render(
