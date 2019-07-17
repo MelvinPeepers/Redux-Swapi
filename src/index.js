0e3b863af4ba202b9fbe743487e04023e4e95ff7
Melvin Peepers <peepersm@gmail.com>
2019-07-16 20:35:00 -0400

    added thunk and set it up on index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
+import "./index.css";
+import App from "./App";
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+// applyMiddleware from redux added
+// thunk from redux-thunk added
+// logger from redux-logger add
+// rootReducer from ./reducers added
 // added compose
 
 const store = createStore(
   rootReducer,
   compose(
     /* applyMiddleware goes here */
-    applyMiddleware(logger),
+    applyMiddleware(thunk, logger),
 
     // Redux DevTool
     window.__REDUX_DEVTOOLS_EXTENSION__
