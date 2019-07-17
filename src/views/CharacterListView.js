921d5c4c600304865c5245b8375690fd7d51a319
Melvin Peepers <peepersm@gmail.com>
2019-07-17 00:05:57 -0400

    setup characterlistview.js

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -18,6 +18,7 @@ class CharacterListView extends React.Component {
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <h2>Retrieving your character, please wait...</h2>;
     }
     return (
       <div className='CharactersList_wrapper'>
@@ -29,9 +30,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.characters
+  };
+};
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps /* mapStateToProps replaces null here */,
+  { getCharacters }
+  /* action creators go here */
 )(CharacterListView);
